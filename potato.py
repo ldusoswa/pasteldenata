03dd7b3b230d6deaa9adca0ddeb9f5c6ab171213
Dusoswa <laurence.dusoswa@dell.com>
2024-01-27 20:04:13 +0000

    latest version

--- potato.py
+++ potato.py
@@ -4,14 +4,19 @@ import glob
 
 from datetime import datetime, timedelta
 
-myTwitchName = 'ldusoswa'
+myTwitchName = 'randomcallsign'
+lvl1Name = 'Random fan'
+lvl2Name = 'RC Garage Crew'
+currency = '€'
+lvl1Cost = 3.99
+lvl2Cost = 4.99
+twitchCost = 4.99
+subListsDir = 'C:\\Users\\dusosl\\Downloads\\'
 
-pitCrewCombined = []
-crewChiefCombined = []
-teamBossCombined = []
-pitCrewYouTube = []
-crewChiefYouTube = []
-teamBossYouTube = []
+lvl1Combined = []
+lvl2Combined = []
+lvl1YouTube = []
+lvl2YouTube = []
 twitchSubs = []
 twitchPrimeExpiryBlurb = []
 totalMemberCount = 0
@@ -19,8 +24,6 @@ totalMemberCount = 0
 totalGross = float(0)
 totalPlatformCosts = float(0)
 totalNet = float(0)
-
-subListsDir = 'C:\\Users\\dusosl\\Downloads\\'
 
 def find_recent_file(dir, prefix):
   # Get the list of all files in the directory
@@ -32,7 +35,7 @@ def find_recent_file(dir, prefix):
   # Return the most recent file
   return file_list[-1]
 
-youtubeSubsFile = find_recent_file(subListsDir, 'Your members ')
+youtubeSubsFile = find_recent_file(subListsDir, 'Your_members')
 twitchSubsFile = find_recent_file(subListsDir, 'subscriber-list')
 
 # quick and dirty replace of poor imports or name change requests
@@ -79,39 +82,18 @@ with open(youtubeSubsFile, 'r') as csv_file:
     sortedlist = sorted(reader, key=lambda row:float(row[4]), reverse=True)
 
     for row in sortedlist:
-        if row[2] == "Pit Crew":
-            pitCrewCombined.append(performTextReplacements(row[0]))
-            pitCrewYouTube.append(performTextReplacements(row[0]))
-        elif row[2] == "Crew Chief":
-            crewChiefCombined.append(performTextReplacements(row[0]))
-            crewChiefYouTube.append(performTextReplacements(row[0]))
-        elif row[2] == "Team Boss":
-            teamBossCombined.append(performTextReplacements(row[0]))
-            teamBossYouTube.append(performTextReplacements(row[0]))
+        if row[2] == lvl1Name:
+            lvl1Combined.append(performTextReplacements(row[0]))
+            lvl1YouTube.append(performTextReplacements(row[0]))
+        elif row[2] == lvl2Name:
+            lvl2Combined.append(performTextReplacements(row[0]))
+            lvl2YouTube.append(performTextReplacements(row[0]))
 
-# output the complete list of names
-print(f'\n#### All Members ####')
-for member in teamBossCombined:
-    print(member)
+totalMemberCount = len(lvl1Combined) + len(lvl2Combined) + len(twitchSubs)
 
-for member in crewChiefCombined:
-    print(member)
-
-print('\n')
-
-for member in pitCrewCombined:
-    print(member)
-
-for member in twitchSubs:
-    print(member)
-
-totalMemberCount = len(pitCrewCombined) + len(crewChiefCombined) + len(teamBossCombined) + len(twitchSubs)
-
-# TODO print out youtube description blurb
-print(f'\nTeam Boss (€19.99/mo)\t\t{", ".join(teamBossCombined)}')
-print(f'Crew Chief (€9.99/mo)\t\t{", ".join(crewChiefCombined)}')
-print(f'Pit Crew (€4.99/mo)\t\t{", ".join(pitCrewCombined)}')
-print(f'TWITCH (€4.99/mo)\t\t{", ".join(twitchSubs)}')
+print(f'{lvl2Name} ({currency}{lvl2Cost}/mo)\t\t{", ".join(lvl2Combined)}')
+print(f'{lvl1Name} ({currency}{lvl1Cost}/mo)\t\t{", ".join(lvl1Combined)}')
+print(f'TWITCH ({currency}{twitchCost}/mo)\t\t{", ".join(twitchSubs)}')
 
 # Create the csv for photoshop to import
 def formatForPhotoshopText(membersArray, padding):
@@ -122,12 +104,11 @@ def formatForPhotoshopText(membersArray, padding):
     return photoshopText
 
 data = [
-    ['teamBoss', 'crewChief', 'pitCrew', 'twitchSubs'],
+    ['lvl2', 'lvl1', 'twitchSubs'],
     [
-        formatForPhotoshopText(teamBossCombined, 38),
-        formatForPhotoshopText(crewChiefCombined, 35),
-        formatForPhotoshopText(pitCrewCombined, 36),
-        formatForPhotoshopText(twitchSubs, 30)
+        formatForPhotoshopText(lvl2Combined, 0),
+        formatForPhotoshopText(lvl1Combined, 0),
+        formatForPhotoshopText(twitchSubs, 0)
     ]
 ]
 psdName = 'levels.csv'
@@ -147,11 +128,6 @@ def calculateAndOutputTotals(platform, tierName, members, pricePerMonth):
         monthlyPlatformFees = monthlyGross*30/100
     elif platform == 'Twitch':
         monthlyPlatformFees = monthlyGross*50/100
-    elif platform == 'Patreon':
-        if pricePerMonth < 3:
-            monthlyPlatformFees = monthlyGross*5/100 + len(members)*0.10
-        else:
-            monthlyPlatformFees = monthlyGross*2.9/100 + len(members)*0.30
     else:
         monthlyPlatformFees = 0
     monthlyNet = monthlyGross - monthlyPlatformFees
@@ -166,19 +142,18 @@ def calculateAndOutputTotals(platform, tierName, members, pricePerMonth):
 
     # spit out the data
     if len(members) > 0:
-        print(f'{platform} - {tierName}\t{len(members)}\t€{pricePerMonth}\t€{"{:.2f}".format(monthlyGross)}\t\t€{"{:.2f}".format(monthlyPlatformFees)}\t\t€{"{:.2f}".format(monthlyNet)}')
+        print(f'{platform} - {tierName}\t\t{len(members)}\t€{pricePerMonth}\t€{"{:.2f}".format(monthlyGross)}\t\t€{"{:.2f}".format(monthlyPlatformFees)}\t\t€{"{:.2f}".format(monthlyNet)}')
 
 # Do earnings projections
 print(f'\n############################# MONTHLY EARNINGS REPORT #####################################')
-print(f'level\t\t\tmembers\trate\tgross income\tplatform costs\ttotal (before tax)')
-print(f'_____________________\t______\t______\t______________\t______________\t__________________')
-calculateAndOutputTotals('YouTube', 'Team Boss', teamBossYouTube, 19.99)
-calculateAndOutputTotals('YouTube', 'Crew Chief', crewChiefYouTube, 9.99)
-calculateAndOutputTotals('YouTube', 'Pit Crew', pitCrewYouTube, 4.99)
-calculateAndOutputTotals('Twitch', 'Subscriptions', twitchSubs, 4.99)
+print(f'level\t\t\t\tmembers\trate\tgross income\tplatform costs\ttotal (before tax)')
+print(f'____________________________\t______\t______\t______________\t______________\t__________________')
+calculateAndOutputTotals('YouTube', lvl2Name, lvl2YouTube, lvl2Cost)
+calculateAndOutputTotals('YouTube', lvl1Name, lvl1YouTube, lvl1Cost)
+calculateAndOutputTotals('Twitch', 'Subscriptions', twitchSubs, twitchCost)
 
 print('===========================================================================================')
-print(f'TOTAL\t\t\t{totalMemberCount}\t\t€{"{:.2f}".format(totalGross)}\t\t€{"{:.2f}".format(totalPlatformCosts)}\t\t€{"{:.2f}".format(totalNet)}')
+print(f'TOTAL\t\t\t\t{totalMemberCount}\t\t€{"{:.2f}".format(totalGross)}\t\t€{"{:.2f}".format(totalPlatformCosts)}\t\t€{"{:.2f}".format(totalNet)}')
 print(f'###########################################################################################')
 
 print(f'\n\n############################# TWITCH PRIME EXPIRING SOON #####################################')
